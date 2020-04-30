d4f01fc696e910b6fe31838f1f3afa4615fadc71
zachbrown12 <59750623+zachbrown12@users.noreply.github.com>
2020-04-30 15:37:44 -0400

    Update run.rb

--- run.rb
+++ run.rb
@@ -5,14 +5,14 @@ require 'json'
 
 # API Key
 # TicketMaster
-# Consumer Key: wid6UhVnt7EDAmE2zohmjVgs6Cw4pCpy
+# 
 
 # SeatGeek
-# Client ID:  MjA5MTgwMjd8MTU4Mjc1NzEwNy4yNw
+# 
 
-tmresults = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=wid6UhVnt7EDAmE2zohmjVgs6Cw4pCpy")
+tmresults = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&
 tmparse_data = JSON.parse(tmresults)
-sgresults = RESTClient.get("https://api.seatgeek.com/2/events?client_id=MjA5MTgwMjd8MTU4Mjc1NzEwNy4yNw")
+sgresults = RESTClient.get("https://api.seatgeek.com/2/events
 sgparse_data - JSON.parse(sgresults)
 
 binding.pry
